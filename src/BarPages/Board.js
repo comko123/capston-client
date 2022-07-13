986de4cd9ea1f75b22e1c82177d6df98b5f00d23
Minseok Kim <comko123@naver.com>
2022-07-13 14:57:09 +0900

    UI blocking

--- src/BarPages/Board.js
+++ src/BarPages/Board.js
@@ -4,6 +4,12 @@ const board = () =>{
 
         return (<>
         <h1>게시판</h1>
+        <br/>
+        <span>
+        <span>더 살펴보기</span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>글쓰기</span>
+        </span>
        <MenuBar/>
         </>)
 
--- src/BarPages/InfoToWeather.js
+++ src/BarPages/InfoToWeather.js
@@ -1,9 +1,16 @@
 import MenuBar from "../Bar/MenuBar";
 const InfoToWeather = () => {
 
-
         return(
-        <>
+        <>{
+                sessionStorage.getItem("login_information")?
+                null
+                :
+                <>
+                성별<br/><input type = "checkbox"/>여성<br/>
+                <input type = "checkbox"/>남성
+                </>
+        }
         <h1>날씨 정보로 추천받기</h1>
         <MenuBar/>
         </>
