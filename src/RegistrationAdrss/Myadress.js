5526f82d48be1c44821004b9c5221ead8ad2bc4f
Minseok Kim <comko123@naver.com>
2022-07-17 00:35:46 +0900

    delete

--- src/RegistrationAdrss/Myadress.js
+++ src/RegistrationAdrss/Myadress.js
@@ -1,23 +1,66 @@
 import MenuBar from "../Bar/MenuBar";
-import RegistrationFall from "./RegistrationFall";
-import RegistrationSpring from "./RegistrationSpring";
-import RegistrationStyle from "./RegistrationStyle";
-import RegistrationSummer from "./RegistrationSummer";
-import RegistrationWinter from "./RegistrationWinter";
+
 const Myadress = () => {
 return(
 <>
 <h3>자신의옷 등록하기</h3>
 <h5>마이페이지&gt;자신의옷 등록하기</h5>
-<RegistrationSpring/>
+<h1>봄</h1>
+    청바지 <input type = "checkbox" value = "spring-jeans"/>
+    <br/>
+    맨투맨 <input type = "checkbox" value = "spring-pullover"/>
+    <br/>
+    셔츠/블라우스 <input type = "checkbox" value = "spring-shirt"/>
+    <br/>
+    후드티 <input type = "checkbox" value = "spring-hoodie"/>
+    <br/>
+    가디건 <input type = "checkbox" value = "spring-Cardigan"/>
 <br/>
-<RegistrationSummer/>
+<h1>여름</h1>
+반바지 <input type = "checkbox" value = "summer-shorts"/>
 <br/>
-<RegistrationFall/>
+반팔티셔츠 <input type = "checkbox" value = "summer-shortsleeve"/>
 <br/>
-<RegistrationWinter/>
+셔츠/블라우스 <input type = "checkbox" value = "summer-shirt"/>
 <br/>
-<RegistrationStyle/>
+민소매티셔츠 <input type = "checkbox" value = "summer-Sleeveless"/>
+<br/>
+청바지 <input type = "checkbox" value = "summer-jeans"/>
+<br/>
+<h1>가을</h1>
+청바지 <input type = "checkbox" value = "fall-jeans"/>
+<br/>
+맨투맨 <input type = "checkbox" value = "fall-pullover"/>
+<br/>
+셔츠/블라우스 <input type = "checkbox" value = "fall-shirt"/>
+<br/>
+후드티 <input type = "checkbox" value = "fall-hoodie"/>
+<br/>
+니트/스웨터 <input type = "checkbox" value = "fall-Cardigan"/>
+<br/>
+<h1>겨울</h1>
+청바지 <input type = "checkbox" value = "winter-jeans"/>
+<br/>
+코트 <input type = "checkbox" value = "winter-coat"/>
+<br/>
+패딩 <input type = "checkbox" value = "winter-padding"/>
+<br/>
+후드티 <input type = "checkbox" value = "winter-hoodie"/>
+<br/>
+니트/스웨터 <input type = "checkbox" value = "winter-knit"/>
+<br/>
+<h1>스타일</h1>
+빈티지 <input type = "checkbox" value = "vintage"/>
+<br/>
+댄디 <input type = "checkbox" value = "dandy"/>
+<br/>
+캐주얼 <input type = "checkbox" value = "casual"/>
+<br/>
+놈코어 <input type = "checkbox" value = "normcore"/>
+<br/>
+스트릿 <input type = "checkbox" value = "street"/>
+<br/>
+<input type = "button" value = "submit" onClick={()=>  window.location.reload()}/>
 <MenuBar/>
 </>
 
