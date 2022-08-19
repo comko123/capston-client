f2a08c40131bcdafe67462dfe8adf66af4e29f7c
Minseok Kim <comko123@naver.com>
2022-08-20 02:28:59 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 /*eslint-disable*/
-import Userdata from "./Userdata";
 import IdFind from "./Find/IdFind";
 import ErrorPages from "./ErrorPages";
 import Mypage from "./MyPage/Mypage";
 import Board from "./BarPages/Board";
 import Sign_up from "./LogInAndSignUp/Sign_up";
+import { Routes, Route } from "react-router-dom";
 import LogoWithMainPage from "./LogoWithMainPage";
 import InfoToMyInfo from "./BarPages/InfoToMyInfo";
-import MyClothing from "./RegistrationClothing/MyClothing";
 import Login_page from "./LogInAndSignUp/Login_page";
 import InfoToWeather from "./BarPages/InfoToWeather";
 import Information from "./ChangeInformatein/Information";
+import MyClothing from "./RegistrationClothing/MyClothing";
 import InfoToMyClothing from "./BarPages/InfoToMyClothing";
-import NewReactiveType from "./ReactiveType/NewReactiveType";
-import { Routes, Route } from "react-router-dom";
 
 const loginState = () => {
     const gett = sessionStorage.getItem("login_information")
     const user = JSON.parse(gett) 
-    return user?.email
-    }
+    return user?.email}
 
 export default function App  (){
 const login = loginState()
 return(
-<NewReactiveType>
-<Userdata>
 <Routes>
 <Route path="*" element = {<ErrorPages/>}/>
 <Route path="/" element={<LogoWithMainPage/>}/>
@@ -39,8 +34,4 @@ return(
 <Route path="/weather_coder/UserInformation/:id" element={!!login?<Information/>:<Login_page/>}/>
 <Route path="/weather_code/Recommendation/Information" element={!!login?<InfoToMyInfo/>:<Login_page/>}/>
 <Route path="/weather_code/Recommendation/Clothing" element={!!login?<InfoToMyClothing/>:<Login_page/>}/>
-</Routes>
-</Userdata>
-</NewReactiveType>
-);
-}
+</Routes>);}
