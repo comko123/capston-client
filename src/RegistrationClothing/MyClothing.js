6c8a3f0691ce41b526c801d622ce6b340cec20ae
Minseok Kim <comko123@naver.com>
2022-09-07 00:30:17 +0900

    grammar change

--- src/RegistrationClothing/MyClothing.js
+++ src/RegistrationClothing/MyClothing.js
@@ -2,13 +2,14 @@
 import React,{useState} from "react";
 import MenuBar from "../Bar/MenuBar";
 const userClothing = {spring:[],summer:[],fall:[],winter:[],style:[]}
-const tik = "/"
+console.log(userClothing[userClothing.style])
+const slash = "/"
  const userSeasonStyle = (...rest) =>{
 return(<>
 <h3 style={{"margin":"0px"}}>{rest[0]}</h3>
 {rest[1].map((clothing,index)=>{
 return (<div key = {index}>{clothing}<input type ="checkbox" 
-value = {rest[0]+tik+`${clothing}`} onClick ={(e)=>
+value = {rest[0]+slash+`${clothing}`} onClick ={e=>
   {if(e.target.checked===true){
    rest[2].push(e.target.value)  
   rest[2].filter((element,index)=>rest[2].indexOf(element)===index)}
@@ -20,17 +21,16 @@ const MyClothing = () => {
   const [fall] =useState(["청바지","맨투맨","셔츠/블라우스","후드티","니트/스웨터"])
   const [winter] =useState(["청바지","코트","패딩","후드티","니트/스웨터"])
   const [style] =useState(["빈티지","댄디","캐주얼","놈코어","스트릿"])
+  const userSave = Object.keys(userClothing) 
   return (
     <><h3>자신의옷 등록하기</h3>
 <h5>마이페이지&gt;자신의옷 등록하기</h5>
 <form onSubmit = {e=>e.preventDefault()}>
-  <div style = {{backgroundColor:"orange"}}>
-{userSeasonStyle("봄",spring,userClothing.spring)}
-</div>
-{userSeasonStyle("여름",summer,userClothing.summer)}
-{userSeasonStyle("가을",fall,userClothing.fall)}
-{userSeasonStyle("겨울",winter,userClothing.winter)}
-{userSeasonStyle("스타일",style,userClothing.style)}
+{userSeasonStyle("봄",spring,userClothing[userSave[0]])}
+{userSeasonStyle("여름",summer,userClothing[userSave[1]])}
+{userSeasonStyle("가을",fall,userClothing[userSave[2]])}
+{userSeasonStyle("겨울",winter,userClothing[userSave[3]])}
+{userSeasonStyle("스타일",style,userClothing[userSave[4]])}
 <input type="button" value = "click" onClick={()=>console.log(userClothing)}/>
   </form>
 <MenuBar/>
