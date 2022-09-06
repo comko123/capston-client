f57d54a5ae632c13d159ebc30d4d926687551077
Minseok Kim <comko123@naver.com>
2022-09-06 20:06:49 +0900

    :)

--- src/LogoWithMainPage.js
+++ src/LogoWithMainPage.js
@@ -1,13 +1,13 @@
 /*eslint-disable*/
 import { Link } from "react-router-dom"
-import { useState,useEffect } from "react";
-import MenuBar from "./Bar/MenuBar";
-import WeatherComponents from "./Weather/WeatherComponents";
+import { useState,useEffect } from "react"
+import MenuBar from "./Bar/MenuBar"
+import WeatherComponents from "./Weather/WeatherComponents"
 const LogoWithMainPage =()=>{
     const [logo,setLogo] = useState(true)
     useEffect(()=>{if(!!!sessionStorage.getItem("logo")){
     setTimeout(()=>{setLogo(false)},3000)
-    if(logo){sessionStorage.setItem("logo","checked");}
+    if(logo){sessionStorage.setItem("logo","checked")}
     }
     else{setLogo(false)}},[])
     return(
@@ -18,9 +18,6 @@ const LogoWithMainPage =()=>{
     <span><Link to = {`/weather_coder/My_page`}>마이 페이지</Link></span>
     <br/>
     <WeatherComponents/> 
-    <br/>
-    <MenuBar/>
-    </>
-    }</>)
+    <br/><MenuBar/></>}</>)
 }
 export default LogoWithMainPage;
