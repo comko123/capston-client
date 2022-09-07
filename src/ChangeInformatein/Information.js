457da3f89e98c38c6fea81362d7aaa3932a8775e
Minseok Kim <comko123@naver.com>
2022-09-08 00:16:27 +0900

    refactoring

--- src/ChangeInformatein/Information.js
+++ src/ChangeInformatein/Information.js
@@ -1,33 +1,28 @@
-/*eslint-disable*/
-import React,{useState} from "react";
+import {userClothingModify,infoUser} from "../data"
 import MenuBar from "../Bar/MenuBar";
-const userClothing = {springObj:{spring:[]},summerObj:{summer:[]},
-fallObj:{fall:[]},winterObj:{winter:[]},styleObj:{style:[]}}
  const userSeasonStyle = (...rest) =>{
+    const sla = "/"
 return(<>
 <h3>{rest[0]}</h3>
 <form onSubmit = {e=>e.preventDefault()}>
 <input type="button" value = "제출" onClick={()=>console.log(rest[3])}/><br/>
 {rest[1].map((clothing,index)=>{
-return (<div key = {index}>{clothing}<input type ="checkbox" value = {rest[0]+"/"+`${clothing}`} onClick ={(e)=>
+return (<div key = {index}>{clothing}<input type ="checkbox" value = {rest[0]+sla+`${clothing}`} onClick ={(e)=>
   {if(e.target.checked===true){
    rest[2].push(e.target.value)  
   rest[2].filter((element,index)=>rest[2].indexOf(element)===index)}
     else{rest[2].splice(rest[2].indexOf(e.target.value),1)}
 }}/> <br/></div>)})}</form></>)}
 export default function  Information() {
-  const [spring] = useState(["청바지","맨투맨","셔츠/블라우스","후드티","가디건"])
-  const [summer] =useState(["반바지","반팔 티셔츠","셔츠/블라우스","민소매티셔츠","청바지"])
-  const [fall] =useState(["청바지","맨투맨","셔츠/블라우스","후드티","니트/스웨터"])
-  const [winter] =useState(["청바지","코트","패딩","후드티","니트/스웨터"])
-  const [style] =useState(["빈티지","댄디","캐주얼","놈코어","스트릿"])
-  return (<><h3>내정보 수정하기</h3>
+    const userMof = Object.keys(userClothingModify) 
+    const userInfo = Object.keys(infoUser)
+    return (<><h3>내정보 수정하기</h3>
 <h5>마이페이지&gt;내정보 수정하기</h5>
-{userSeasonStyle("봄" , spring , userClothing.springObj.spring , userClothing.springObj)}
-{userSeasonStyle("여름" , summer , userClothing.summerObj.summer , userClothing.summerObj)}
-{userSeasonStyle("가을" , fall,userClothing.fallObj.fall , userClothing.fallObj)}
-{userSeasonStyle("겨울" , winter , userClothing.winterObj.winter , userClothing.winterObj)}
-{userSeasonStyle("스타일" , style , userClothing.styleObj.style , userClothing.styleObj)}
+{userInfo.map((item,index)=>{
+    const season =userClothingModify[userMof[index]]
+    return <div key={index}>
+    {userSeasonStyle(userInfo[index],infoUser[item],season[Object.keys(season)],season)}
+</div>})}
 <br/>
 <form onSubmit = {e=>e.preventDefault()}>
 <h3>비밀번호 변경</h3>
@@ -40,7 +35,6 @@ export default function  Information() {
 <br/>
 <input type = "button" value = "비밀번호 수정"/>
 <br/>
-
 </form>
 <MenuBar/>
-    </>);}
+    </>)}
--- src/RegistrationClothing/MyClothing.js
+++ src/RegistrationClothing/MyClothing.js
@@ -1,10 +1,6 @@
-
-import React,{useState} from "react";
-import MenuBar from "../Bar/MenuBar";
-const userClothing = {spring:[],summer:[],fall:[],winter:[],style:[]}
-console.log(userClothing[userClothing.style])
+import {infoUser,userClothingRegistration} from "../data"
 const slash = "/"
- const userSeasonStyle = (...rest) =>{
+const userSeasonStyle = (...rest) =>{
 return(<>
 <h3 style={{"margin":"0px"}}>{rest[0]}</h3>
 {rest[1].map((clothing,index)=>{
@@ -16,26 +12,21 @@ value = {rest[0]+slash+`${clothing}`} onClick ={e=>
     else{rest[2].splice(rest[2].indexOf(e.target.value),1)}
 }}/> <br/></div>)})}</>)}
 const MyClothing = () => {
-  const [spring] = useState(["청바지","맨투맨","셔츠/블라우스","후드티","가디건"])
-  const [summer] =useState(["반바지","반팔 티셔츠","셔츠/블라우스","민소매티셔츠","청바지"])
-  const [fall] =useState(["청바지","맨투맨","셔츠/블라우스","후드티","니트/스웨터"])
-  const [winter] =useState(["청바지","코트","패딩","후드티","니트/스웨터"])
-  const [style] =useState(["빈티지","댄디","캐주얼","놈코어","스트릿"])
-  const userSave = Object.keys(userClothing) 
+  const userSave = Object.keys(userClothingRegistration) 
+  const userIn = Object.keys(infoUser)
   return (
     <><h3>자신의옷 등록하기</h3>
 <h5>마이페이지&gt;자신의옷 등록하기</h5>
 <form onSubmit = {e=>e.preventDefault()}>
-{userSeasonStyle("봄",spring,userClothing[userSave[0]])}
-{userSeasonStyle("여름",summer,userClothing[userSave[1]])}
-{userSeasonStyle("가을",fall,userClothing[userSave[2]])}
-{userSeasonStyle("겨울",winter,userClothing[userSave[3]])}
-{userSeasonStyle("스타일",style,userClothing[userSave[4]])}
-<input type="button" value = "click" onClick={()=>console.log(userClothing)}/>
+    {userIn.map((item,index)=>{return <div style ={{margin:"20px"}} key={index}>{
+    userSeasonStyle(userIn[index],infoUser[item],userClothingRegistration[userSave[index]])}</div>})}
+<input type="button" value = "click" onClick={()=>console.log(userClothingRegistration)}/> 
   </form>
-<MenuBar/>
     </>
   );
 }
 
 export default MyClothing;
+
+
+
