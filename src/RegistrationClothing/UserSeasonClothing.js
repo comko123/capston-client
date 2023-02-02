7c81b2e8f0b8367a5f6124e7bd8dee81327524b4
Minseok Kim <comko123@naver.com>
2023-02-03 00:55:58 +0900

    refactoring...

--- src/RegistrationClothing/UserSeasonClothing.js
+++ src/RegistrationClothing/UserSeasonClothing.js
@@ -1,6 +1,9 @@
 import UserSelectorList from "./UserSelectorList"
 const UserSeasonClothing = ({season,seasonList,storeText,includeElement}) => {
-return (<div className={"Myadress_main_component"}><h3 style={{"margin": "0px"}} className={"Myadress_main_component_title"}>{season}</h3>
-{seasonList.map((clothing,number) => <UserSelectorList key={clothing}value={seasonList[number]} includeElement={includeElement}
+return (
+<div className={"Myadress_main_component"}>
+<h3 style={{"margin": "0px"}} className={"Myadress_main_component_title"}>{season}</h3>
+{seasonList.map((clothing,number) => <UserSelectorList 
+key={clothing}value={seasonList[number]} includeElement={includeElement}
 season={season} clothing ={clothing} list={storeText}/>)}</div>)}
 export default UserSeasonClothing  
