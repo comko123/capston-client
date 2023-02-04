83af8774a072e6a91379a7ae40f2b574a2124753
Minseok Kim <comko123@naver.com>
2023-02-05 00:01:06 +0900

    .

--- src/ChangeInformation/UserCheck.js
+++ src/ChangeInformation/UserCheck.js
@@ -3,8 +3,10 @@ import { useReduxClallBack } from "../hooks/useReduxClallBack"
 
 const UserCheck = ({list,value,number,styleList}) => {
 const {add,dpc,rmv} = useReduxClallBack({list,value,index:1})
-return(<div className={"Myadress_main_component_items"}>
-<input className={"Myadress_main_component_item_check"} type="checkbox" value = {styleList[number]}
-onClick ={e=>{if(e.target.checked){add();dpc}else{rmv()}}}/>
-<span className={"Myadress_main_component_item_text"}>{value}</span></div>)}
+return(
+<div className={"Myadress_main_component_items"}>
+    <input className={"Myadress_main_component_item_check"} type="checkbox" value = {styleList[number]}
+        onClick ={e=>{if(e.target.checked){add();dpc}else{rmv()}}}/>
+<span className={"Myadress_main_component_item_text"}>{value}
+</span></div>)}
 export default UserCheck
--- src/ChangeInformation/UserSeasonStyle.js
+++ src/ChangeInformation/UserSeasonStyle.js
@@ -3,13 +3,16 @@ import {certifiedToken, parsingLogic } from "../data"
 import UserCheck from "./UserCheck"
 const UserSeasonStyle = ({title,styleList,storeText,storeStyleList,result }) => {
 const infoModify = inCloser.useSetMyInfoMutation()
-return (<div className={"Myadress_main_component"}>
+return (
+<div className={"Myadress_main_component"}>
 <div className={"Information_main_component_title_container"}>
 <h3 className={"Myadress_main_component_title Information_main_component_title"}>{title}</h3>
-</div><form onSubmit = {async(e)=>{e.preventDefault()
+</div>
+<form onSubmit = {async(e)=>{e.preventDefault()
 const resultData =  await(await infoModify[0]({token:{Authorization:`Bearer ${certifiedToken}`},
 info:storeStyleList[1].style,settingUrl:`/my-style`})).data
 try {parsingLogic(result,resultData)} catch (error) {console.log(error)}}}>
 {styleList.map((clothing, number) => <UserCheck key={number} list={storeText} value={clothing} number={number}styleList={styleList}/>)}
-<input className={"Information_component_submit"} type="submit" value="submit"/></form></div>)}
+<input className={"Information_component_submit"} type="submit" value="submit"/>
+</form></div>)}
 export default UserSeasonStyle
