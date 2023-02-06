62ca9d619eed5f1f182c761e92537672e44c8e18
Minseok Kim <comko123@naver.com>
2023-02-07 01:50:30 +0900

    refactoring

--- src/Bar/MenuBar.js
+++ src/Bar/MenuBar.js
@@ -12,17 +12,31 @@ import menu_weather_on from "../img/menu_weather_on.jpg"
 import menu_home_off from "../img/menu_home_off.jpg"
 import menu_board_on from "../img/menu_board_on.jpg"
 const parm = (para) =>sessionStorage.getItem("login_information")? 
-window.location.replace(`/Recommendation/${para}`):!alert("로그인후 이용이 가능한 서비스 입니다.") && window.location.replace(`/Login`)
-const MenuBar = () => {const {pathname,search} = useLocation()
-return (<div className={"bottom-nav"}>
+window.location.replace(`/Recommendation/${para}`):
+!alert("로그인후 이용이 가능한 서비스 입니다.") && window.location.replace(`/Login`)
+const MenuBar = () => {
+    const {pathname,search} = useLocation()
+return (
+<div className={"bottom-nav"}>
+
 <h3 className={"bn-tab"} onClick={() => window.location.replace(`/Recommendation/weather`)}>
-<img src={pathname==="/Recommendation/weather"?menu_weather_on:menu_weather} width="24" height="18" alt="날씨 정보로 추천받기"/></h3>
+<img src={pathname==="/Recommendation/weather"?menu_weather_on:menu_weather} 
+width="24" height="18" alt="날씨 정보로 추천받기"/></h3>
+
 <h3 className={"bn-tab"} onClick={() => parm("Information")}>
-<img src={pathname==="/Recommendation/Information"?menu_me_on:menu_me} width="20" height="20" alt="내 정보로 추천받기"/></h3>
+<img src={pathname==="/Recommendation/Information"?menu_me_on:menu_me} 
+width="20" height="20" alt="내 정보로 추천받기"/></h3>
+
 <h3 className={"bn-tab"} onClick={() => window.location.replace(`/`)}>
-<img src={pathname==="/"?menu_home:menu_home_off} width="21.25" height="20.07" alt="현재 날씨 조회"/></h3>
+<img src={pathname==="/"?menu_home:menu_home_off} 
+width="21.25" height="20.07" alt="현재 날씨 조회"/></h3>
+
 <h3 className={"bn-tab"} onClick={() => parm("Clothing")}>
-<img src={pathname==="/Recommendation/Clothing"?menu_cloth_on:menu_cloth} width="24" height="22" alt="내 옷들로 추천받기"/></h3>
+<img src={pathname==="/Recommendation/Clothing"?menu_cloth_on:menu_cloth} 
+width="24" height="22" alt="내 옷들로 추천받기"/></h3>
+
 <h3 className={"bn-tab"} onClick={() => window.location.replace(`/Border/route/0`)}>
-<img src={isNaN(+pathname.split("")[14])?!search?menu_board:menu_board_on:menu_board_on} width="22" height="22" alt="게시판"/></h3></div>)}
+<img src={isNaN(+pathname.split("")[14])?!search?menu_board:menu_board_on:menu_board_on} 
+width="22" height="22" alt="게시판"/></h3>
+</div>)}
 export default React.memo(MenuBar)
