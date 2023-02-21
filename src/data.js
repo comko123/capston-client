1863256f2b2719ac1d6526370fce02bf67d0ea7a
Minseok Kim <comko123@naver.com>
2023-02-22 02:02:16 +0900

    refactoring

--- src/data.js
+++ src/data.js
@@ -8,29 +8,37 @@ export const infoUser = {
 봄:["니트/스웨터","맨투맨","셔츠/블라우스","후드티","가디건"],
 여름:["반팔 티셔츠","셔츠/블라우스","민소매 티셔츠","반바지","청바지"],
 가을:["청바지","맨투맨","셔츠/블라우스","후드티","니트/스웨터"],
-겨울:["청바지","코트","패딩","후드티","니트/스웨터"]}
+겨울:["청바지","코트","패딩","후드티","니트/스웨터"]
+        }
 
 export const selectUserData = {
 연령: ["10 ~ 19세","20 ~ 29세","30 ~ 39세","40 ~ 49세","50세 이상"],
 성별:["남성","여성"],
 신장: ["156 ~ 160cm","160 ~ 165cm","165 ~ 170cm","170 ~ 175cm","175이상"],
-체중: ["45 ~ 55kg","56 ~ 65kg","66 ~ 75kg","76 ~ 85kg","85kg 이상"]}
+체중: ["45 ~ 55kg","56 ~ 65kg","66 ~ 75kg","76 ~ 85kg","85kg 이상"]
+        }
 
 export class ruby {
 constructor(lowTemp,highTemp){
 this.lowTemp = lowTemp
-this.highTemp = highTemp}}
+this.highTemp = highTemp
+        }
+            }
 
 export class selectDataParse{
 constructor(seasonName,nameList){
 this.seasonName=seasonName
-this.nameList=nameList}}
+this.nameList=nameList
+        }
+            }
 
 export class infotomyinfo{
 constructor(...rest){
 this.lowTemp=rest[0]
 this.highTemp=rest[1]
-this.gender=rest[2]}}
+this.gender=rest[2]
+        }
+            }
 
 export class signInfo {
 constructor(...rest){
@@ -40,7 +48,9 @@ this.gender = rest[2]
 this.age = rest[3]
 this.height = rest[4]
 this.weight = rest[5]
-this.styleList = rest[6]}}
+this.styleList = rest[6]
+        }
+            }
 
 export class listText {
 constructor(title,content){
