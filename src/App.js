324b5f19c33ba2c66ff396a6046cab5369c1c147
Minseok Kim <comko123@naver.com>
2023-04-07 00:01:42 +0900

    리펙토링 사항 추가

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
 /*eslint-disable*/
-import { Routes, Route, useNavigate} from "react-router-dom"
-import { useEffect , lazy ,Suspense} from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { useEffect, lazy, Suspense } from "react"
 import LogoWithMainPage from "./LogoWithMainPage"
 import FallBackWating from "./FallBackWating"
-const Mypage= lazy(()=>import("./MyPage/Mypage")) 
-const ErrorPages= lazy(()=>import("./ErrorPages")) 
-const Write= lazy(()=>import("./BarPages/Border/Write")) 
-const Comment= lazy(()=>import("./BarPages/Border/Comment")) 
-const InfoToMyInfo= lazy(()=>import("./BarPages/InfoToMyInfo")) 
-const Login_page= lazy(()=>import("./LogInAndSignUp/Login_page")) 
-const InfoToWeather= lazy(()=>import("./BarPages/InfoToWeather")) 
-const PasswordChange= lazy(()=>import("./Change/PasswordChange")) 
-const StyleChange= lazy(()=>import("./ChangeInformation/StyleChange")) 
-const MyClothing= lazy(()=>import("./RegistrationClothing/MyClothing")) 
-const InfoToMyClothing= lazy(()=>import("./BarPages/InfoToMyClothing")) 
-const WritingLetter= lazy(()=>import("./BarPages/Border/WritingLetter")) 
-const Sign_up= lazy(()=>import("./LogInAndSignUp/SignUpSection/Sign_up")) 
-const Border= lazy(()=>import("./BarPages/Border/BorderMainSection/Border")) 
-const BorderSearchPage= lazy(()=>import("./BarPages/Border/BorderSearchSection/BorderSearchPage")) 
+const Mypage = lazy(() => import("./MyPage/Mypage"))
+const ErrorPages = lazy(() => import("./ErrorPages"))
+const Write = lazy(() => import("./BarPages/Border/Write"))
+const Comment = lazy(() => import("./BarPages/Border/Comment"))
+const InfoToMyInfo = lazy(() => import("./BarPages/InfoToMyInfo"))
+const Login_page = lazy(() => import("./LogInAndSignUp/Login_page"))
+const InfoToWeather = lazy(() => import("./BarPages/InfoToWeather"))
+const PasswordChange = lazy(() => import("./Change/PasswordChange"))
+const StyleChange = lazy(() => import("./ChangeInformation/StyleChange"))
+const MyClothing = lazy(() => import("./RegistrationClothing/MyClothing"))
+const InfoToMyClothing = lazy(() => import("./BarPages/InfoToMyClothing"))
+const WritingLetter = lazy(() => import("./BarPages/Border/WritingLetter"))
+const Sign_up = lazy(() => import("./LogInAndSignUp/SignUpSection/Sign_up"))
+const Border = lazy(() => import("./BarPages/Border/BorderMainSection/Border"))
+const BorderSearchPage = lazy(() => import("./BarPages/Border/BorderSearchSection/BorderSearchPage"))
 
 const loginState = () => {
-const gett = sessionStorage.getItem("login_information")
-const user = JSON.parse(gett)
-return user?.username
+    const gett = sessionStorage.getItem("login_information")
+    const user = JSON.parse(gett)
+    return user?.username
 }
 
 export default function App() {
-const login = loginState()
-const navigate = useNavigate()
-useEffect(()=>{sessionStorage.getItem("wheatherInfo")?null:navigate('/')},[])
+    const login = loginState()
+    const navigate = useNavigate()
+    useEffect(() => { sessionStorage.getItem("wheatherInfo") ? null : navigate('/') }, [])
 
-return (
-<Suspense fallback={<FallBackWating/>}><Routes>
-<Route path="*" element={<ErrorPages/>}/>
-<Route path="/Border" element={<Border/>}>
-<Route path="route/:index" element={null}/></Route>
-<Route path="detail/:index" element={<WritingLetter/>}>
-<Route path="Comment/:id" element={<Comment/>}/></Route>
-<Route path="/Border/search/content"element={<BorderSearchPage/>}/>
-<Route path="/detail/write" element={<Write/>}/>     
-<Route path="/My_page" element={<Mypage/>}/>
-<Route path="/Recommendation/weather" element={<InfoToWeather/>}/>
-<Route path="/" element={<LogoWithMainPage/>}/>
-<Route path="/ChangeToPassword" element={<PasswordChange/>}/>
-<Route path="/Sign_up" element={!!login ? <LogoWithMainPage/>:<Sign_up/>}/>
-<Route path="/Login" element={!!login ? <Mypage/>:<Login_page/>}/>
-<Route path="/Input_clothing" element={!!login ? <MyClothing/>:<Login_page/>}/>
-<Route path="/Recommendation/Information" element={!!login ? <InfoToMyInfo/>:<Login_page/>}/>
-<Route path="/Recommendation/Clothing" element={!!login ? <InfoToMyClothing/>:<Login_page/>}/>
-<Route path="/My_page/StyleChange" element={!!login ? <StyleChange />:<Login_page />}/>
-</Routes></Suspense>)}
+    return (
+        <Suspense fallback={<FallBackWating />}><Routes>
+            <Route path="*" element={<ErrorPages />} />
+            <Route path="/Border" element={<Border />}>
+                <Route path="route/:index" element={null} /></Route>
+            <Route path="detail/:index" element={<WritingLetter />}>
+                <Route path="Comment/:id" element={<Comment />} /></Route>
+            <Route path="/Border/search/content" element={<BorderSearchPage />} />
+            <Route path="/detail/write" element={<Write />} />
+            <Route path="/My_page" element={<Mypage />} />
+            <Route path="/Recommendation/weather" element={<InfoToWeather />} />
+            <Route path="/" element={<LogoWithMainPage />} />
+            <Route path="/ChangeToPassword" element={<PasswordChange />} />
+            <Route path="/Sign_up" element={!!login ? <LogoWithMainPage /> : <Sign_up />} />
+            <Route path="/Login" element={!!login ? <Mypage /> : <Login_page />} />
+            <Route path="/Input_clothing" element={!!login ? <MyClothing /> : <Login_page />} />
+            <Route path="/Recommendation/Information" element={!!login ? <InfoToMyInfo /> : <Login_page />} />
+            <Route path="/Recommendation/Clothing" element={!!login ? <InfoToMyClothing /> : <Login_page />} />
+            <Route path="/My_page/StyleChange" element={!!login ? <StyleChange /> : <Login_page />} />
+        </Routes></Suspense>)
+}
--- src/Weather/TodayWeather.js
+++ src/Weather/TodayWeather.js
@@ -2,35 +2,46 @@
 import NowWeather from "./NowWeather"
 import WeatherDisplay from "./WeatherDisplay"
 import WeatherAlgorithm from "./WeatherAlgorithm"
-import React,{useEffect,useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./TodayWeather.moudule.css"
 import { inCloser } from "../api/inClosing"
 
 const temp = (...rest) => {
-const dateValue = new Date()
-const array = rest[0].map((R)=>{const value = new Date(R.dt*1000);
-return dateValue.getDate()!==value.getDate()?null:R.temp })
-const data = array.filter(E=>E!==null)
-const hotdata = Math.max.apply(null,data)
-const colddata = Math.min.apply(null,data)
-rest[1](hotdata);rest[2](colddata)}
+    const dateValue = new Date()
+    const array = rest[0].map((R) => {
+        const value = new Date(R.dt * 1000);
+        return dateValue.getDate() !== value.getDate() ? null : R.temp
+    })
+    const data = array.filter(E => E !== null)
+    const hotdata = Math.max.apply(null, data)
+    const colddata = Math.min.apply(null, data)
+    rest[1](hotdata); rest[2](colddata)
+}
 
 const rain = (...rest) => {
-const dateValue = new Date()
-const rainning =rest[0].map((R)=>{const value = new Date(R.dt*1000)
-return dateValue.getDate()!==value.getDate()?null:R.rain??null })
-const rainData = rainning.filter(E=>E!==null);
-(rainData.length!==0)? rest[1](true): rest[1](false)}
+    const dateValue = new Date()
+    const rainning = rest[0].map((R) => {
+        const value = new Date(R.dt * 1000)
+        return dateValue.getDate() !== value.getDate() ? null : R.rain ?? null
+    })
+    const rainData = rainning.filter(E => E !== null);
+    (rainData.length !== 0) ? rest[1](true) : rest[1](false)
+}
 
-const TodayWeather = ({latitude,longitude,setDate}) =>{
-const [[rainData,setRainData],[lowTemp,setLowTemp],[highTemp,setHighTemp]] = [useState(false),useState(0),useState(0)]
-const {isLoading,data} = inCloser.useGetWheatherQuery({latitude,longitude,state:"onecall"})
-useEffect(() => {if(data?.hourly?.length){temp(data?.hourly,setHighTemp,setLowTemp)
-rain(data?.hourly,setRainData)}},[data?.hourly])
-return (<>{isLoading?null:
-<div className={"TodayWeather_container"}>
-<NowWeather latitude={latitude} longitude = {longitude} setDate={setDate}/>
-{data?.hourly?.map(({dt,humidity,temp,weather})=>
-<WeatherDisplay key={dt} dt={dt} humidity={humidity} temp={temp} weather={weather}/>)}
-<WeatherAlgorithm Htemp = {Math.round(highTemp)} Mtemp={Math.round(lowTemp)} rain={rainData}/></div>}</>)}
+const TodayWeather = ({ latitude, longitude, setDate }) => {
+    const [[rainData, setRainData], [lowTemp, setLowTemp], [highTemp, setHighTemp]] = [useState(false), useState(0), useState(0)]
+    const { isLoading, data } = inCloser.useGetWheatherQuery({ latitude, longitude, state: "onecall" })
+    useEffect(() => {
+        if (data?.hourly?.length) {
+            temp(data?.hourly, setHighTemp, setLowTemp)
+            rain(data?.hourly, setRainData)
+        }
+    }, [data?.hourly])
+    return (<>{isLoading ? null :
+        <div className={"TodayWeather_container"}>
+            <NowWeather latitude={latitude} longitude={longitude} setDate={setDate} />
+            {data?.hourly?.map(({ dt, humidity, temp, weather }) =>
+                <WeatherDisplay key={dt} dt={dt} humidity={humidity} temp={temp} weather={weather} />)}
+            <WeatherAlgorithm Htemp={Math.round(highTemp)} Mtemp={Math.round(lowTemp)} rain={rainData} /></div>}</>)
+}
 export default React.memo(TodayWeather)
