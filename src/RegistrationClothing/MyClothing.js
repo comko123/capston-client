5c2bc88b192f54b13120a3d45f1a4c0f89a77ee3
Minseok Kim <comko123@naver.com>
2023-05-01 16:20:31 +0900

    refactor : 코드 라인 정리 및 스토어 데이터 정리

--- src/RegistrationClothing/MyClothing.js
+++ src/RegistrationClothing/MyClothing.js
@@ -1,42 +1,63 @@
 /*eslint-disable*/
-import { useState,useEffect} from "react"
-import { useSelector} from "react-redux"
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import {selectDataParse,infoUser, certifiedToken, parsingLogic} from "../data"
+import { selectDataParse, infoUser, certifiedToken, parsingLogic } from "../data"
 import MenuBar from "../Bar/MenuBar"
 import pageback_btn from "../img/pageback_btn.png"
 import "./MyClothing.moudule.css"
 import { useAlert } from "../hooks/useAlert"
 import UserSeasonClothing from "./UserSeasonClothing"
 import { useClear } from "../hooks/useClear"
-import {inCloser}  from "../api/inClosing"
+import { inCloser } from "../api/inClosing"
 const MyClothing = () => {
-const selector = useSelector(item=>item.addStyleList)
-const [selectData,userIn] = [Object.keys(selector[0]),Object.keys(infoUser)]
-const [[message,setMessage],[state,setState]] = [useState({}),useState([])]
-const [styleModify,navigate] =[inCloser.useSetStyleInfoMutation(),useNavigate()]
-useClear();  useAlert(message,'/')
-useEffect(()=>{(
-async()=>{if(state.length){
-const resultData =  await(await styleModify[0]({token:{
-Authorization:`Bearer ${certifiedToken}`},info:state , settingUrl:`/my-clothes`})).data
-try {parsingLogic(setMessage,resultData)} catch (error) {console.log(error)}}
-else if(state.length>=5){navigate(0)}})()},[state,navigate])
-return (<><div className={"Myadress_container"}>
-<div className={"Myadress_innercontainer"}>
-<img src={pageback_btn} width={16} height={16} onClick={()=>navigate(-1)} alt="" className={"Myadress_pageback"}/>
-<h3 className={"Myadress_title"}>자신의옷 등록하기</h3>
-<h5 className={"Myadress_description"}>* 회원의 옷을 기반으로 맞춤형 추천을 제공합니다.</h5>
-<form onSubmit={e => {(async()=>{e.preventDefault()
-try {for(const key in selector[0]){
-if(selector[0][key].length){
-const data = new selectDataParse(key,selector[0][key])
-setState(state=>[...state,data])}}} 
-catch (error) {console.log(error)}})()}} className={"Myadress_maincontainer_outer"}>
-<div className={"Myadress_maincontainer"}>
-{userIn.map((item,num)=>{return <UserSeasonClothing 
-season={userIn[num]} seasonList={infoUser[item]} key={userIn[num]} 
-storeText={selectData[num]} includeElement={selector[0]}/>})}</div>
-<input className={"Myadress_submit"} type="submit" value="submit"/>
-</form></div></div><MenuBar/></>)}
+    const selector = useSelector(item => item.addStyleList)
+    const [selectData, userIn] = [Object.keys(selector.no1), Object.keys(infoUser)]
+    const [[message, setMessage], [state, setState]] = [useState({}), useState([])]
+    const [styleModify, navigate] = [inCloser.useSetStyleInfoMutation(), useNavigate()]
+    useClear(); 
+    useAlert(message, '/')
+    useEffect(() => {
+        (
+            async () => {
+                if (state.length) {
+                    const resultData = await (await styleModify[0]({
+                        token: {
+                            Authorization: `Bearer ${certifiedToken}`
+                        }, info: state, settingUrl: `/my-clothes`
+                    })).data
+                    try { parsingLogic(setMessage, resultData) } 
+                    catch (error) { console.log(error) }
+                }
+                else if (state.length >= 5) { navigate(0) }
+            })()
+    }, [state, navigate])
+    return (<><div className={"Myadress_container"}>
+        <div className={"Myadress_innercontainer"}>
+            <img src={pageback_btn} width={16} height={16} onClick={() => navigate(-1)} alt="" className={"Myadress_pageback"} />
+            <h3 className={"Myadress_title"}>자신의옷 등록하기</h3>
+            <h5 className={"Myadress_description"}>* 회원의 옷을 기반으로 맞춤형 추천을 제공합니다.</h5>
+            <form onSubmit={e => {
+                (async () => {
+                    e.preventDefault()
+                    try {
+                        for (const key in selector[0]) {
+                            if (selector[0][key].length) {
+                                const data = new selectDataParse(key, selector[0][key])
+                                setState(state => [...state, data])
+                            }
+                        }
+                    }
+                    catch (error) { console.log(error) }
+                })()
+            }} className={"Myadress_maincontainer_outer"}>
+                <div className={"Myadress_maincontainer"}>
+                    {userIn.map((item, num) => {
+                        return <UserSeasonClothing
+                            season={userIn[num]} seasonList={infoUser[item]} key={userIn[num]}
+                            storeText={selectData[num]} includeElement={selector[0]} />
+                    })}</div>
+                <input className={"Myadress_submit"} type="submit" value="submit" />
+            </form></div></div><MenuBar /></>)
+}
 export default MyClothing
--- src/store.js
+++ src/store.js
@@ -1,14 +1,26 @@
-import {configureStore,createSlice} from "@reduxjs/toolkit"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 import { inCloser } from "./api/inClosing"
 const registration = createSlice({
-name:"registration",initialState:[{봄:[],여름:[],가을:[],겨울:[]},{style:[]},
-{email:[],password:[]},{성별:[],연령:[],신장:[],체중:[],스타일:[]}],
-reducers:{
-addStyle(state,action){state[action.payload.index][action.payload.list].push(action.payload.value)},
-deduplicationStyle(state,action){state[action.payload.index][action.payload.list].filter((element,index)=>state[action.payload.index][action.payload.list].indexOf(element)===index)},
-removeStyle(state,action){state[action.payload.index][action.payload.list].splice(state[action.payload.index][action.payload.list].indexOf(action.payload.value),1)},
-inputLengthStyle(state,action){state[action.payload.index][action.payload.list].splice(0,state[action.payload.index][action.payload.list].length-1)},
-clearStyle(state,action){Object.keys(state[action.payload]).map(item=>state[action.payload][item].splice(0))}}})
-export const{addStyle,removeStyle,deduplicationStyle,clearStyle,inputLengthStyle} = registration.actions
-export default configureStore({reducer:{addStyleList:registration.reducer,[inCloser.reducerPath]:inCloser.reducer},
-middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(inCloser.middleware)})
+    name: "registration", 
+    initialState: {
+        no1:{봄:[],여름:[],가을:[],겨울:[]}, 
+        no2:{style:[]},
+        no3:{email:[],password:[]}, 
+        no4:{성별:[], 연령:[],신장:[],체중:[],스타일:[]}
+    },
+    reducers: {
+        addStyle(state, action) { state[action.payload.index][action.payload.list].push(action.payload.value) },
+        deduplicationStyle(state, action) { state[action.payload.index][action.payload.list].filter((element, index) => state[action.payload.index][action.payload.list].indexOf(element) === index) },
+        removeStyle(state, action) { state[action.payload.index][action.payload.list].splice(state[action.payload.index][action.payload.list].indexOf(action.payload.value), 1) },
+        inputLengthStyle(state, action) { state[action.payload.index][action.payload.list].splice(0, state[action.payload.index][action.payload.list].length - 1) },
+        clearStyle(state, action) { Object.keys(state[action.payload]).map(item => state[action.payload][item].splice(0)) }
+    }
+})
+export const { addStyle, removeStyle, deduplicationStyle, clearStyle, inputLengthStyle } = registration.actions
+export default configureStore({
+    reducer: { addStyleList: registration.reducer, [inCloser.reducerPath]: inCloser.reducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(inCloser.middleware)
+})
+
+/* original : [{봄:[],여름:[],가을:[],겨울:[]},{style:[]},
+{email:[],password:[]},{성별:[],연령:[],신장:[],체중:[],스타일:[]}]*/
