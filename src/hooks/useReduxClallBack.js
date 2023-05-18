43a84c315e293222b830a8a2f513d88b3ba61c88
Minseok Kim <comko123@naver.com>
2023-05-19 00:40:52 +0900

    .

--- src/hooks/useReduxClallBack.js
+++ src/hooks/useReduxClallBack.js
@@ -1,15 +1,16 @@
-/*eslint-disable*/ 
+/*eslint-disable*/
 import { useCallback } from "react"
 import { useDispatch } from "react-redux"
 import { addStyle, deduplicationStyle, removeStyle } from "../store"
-export const useReduxClallBack = ({list,value,index}) => {
-const dispatch = useDispatch()
-const action = {list,value,index:index}
-const addStyleDispatch = useCallback(()=>{dispatch(addStyle(action))},[action,dispatch])
-const deduplicationStyleDispatch = useCallback(()=>{dispatch(deduplicationStyle(action))},[action,dispatch])
-const removeStyleDispatch = useCallback(()=>{dispatch(removeStyle(action))},[action,dispatch])
-return {
-    add:addStyleDispatch,
-    dpc:deduplicationStyleDispatch,
-    rmv:removeStyleDispatch
-}}
+export const useReduxClallBack = ({ list, value, index }) => {
+    const dispatch = useDispatch()
+    const action = { list, value, index: index }
+    const addStyleDispatch = useCallback(() => { dispatch(addStyle(action)) }, [action, dispatch])
+    const deduplicationStyleDispatch = useCallback(() => { dispatch(deduplicationStyle(action)) }, [action, dispatch])
+    const removeStyleDispatch = useCallback(() => { dispatch(removeStyle(action)) }, [action, dispatch])
+    return {
+        add: addStyleDispatch,
+        dpc: deduplicationStyleDispatch,
+        rmv: removeStyleDispatch
+    }
+}
